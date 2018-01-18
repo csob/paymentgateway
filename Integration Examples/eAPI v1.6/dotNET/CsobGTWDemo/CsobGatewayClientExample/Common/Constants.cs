512b22aaa6b94165ccb70c6c7c980fecb985b02a
Tomas Jecha <tomas.jecha@actum.cz>
2018-01-18 13:17:34 +0100

    Renewed test keys and MerchantId

--- Integration Examples/eAPI v1.6/dotNET/CsobGTWDemo/CsobGatewayClientExample/Common/Constants.cs
+++ Integration Examples/eAPI v1.6/dotNET/CsobGTWDemo/CsobGatewayClientExample/Common/Constants.cs
@@ -5,11 +5,11 @@ namespace CsobGatewayClientExample.Common
 {
     public static class Constants
     {
-        public static readonly string PrivateKeyFilePath = Path.Combine(Environment.CurrentDirectory, @"Assets", "rsa_A2169rONf8.key");
-        public static readonly string PublicKeyFilePath = Path.Combine(Environment.CurrentDirectory, @"Assets", "rsa_A2169rONf8.pub");
+        public static readonly string PrivateKeyFilePath = Path.Combine(Environment.CurrentDirectory, @"Assets", "rsa_A3070d3A3V.key");
+        public static readonly string PublicKeyFilePath = Path.Combine(Environment.CurrentDirectory, @"Assets", "rsa_A3070d3A3V.pub");
         public static readonly string PaymentInitBaseFilePath = Path.Combine(Environment.CurrentDirectory, @"Assets", "payment-init-base.json");
         public static readonly string PaymentOneclickBaseFilePath = Path.Combine(Environment.CurrentDirectory, @"Assets", "payment-oneclick-base.json");
         public const string GatewayUrl = "https://iapi.iplatebnibrana.csob.cz/api/v1.6";
-        public const string MerchantId = "A2169rONf8";
+        public const string MerchantId = "A3070d3A3V";
     }
 }
